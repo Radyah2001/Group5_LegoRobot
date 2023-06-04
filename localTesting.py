3c3fccbccdd2c9cbf77fd650bb9b35749279d58c
Haydar Hamid Abbas Bahr <haydar919@gmail.com>
2023-06-04 17:17:37 +0200

    Works now with local model

---
+++ localTesting.py
@@ -0,0 +1,98 @@
+import cv2
+
+from ultralytics import YOLO
+import supervision as sv
+import numpy as np
+import math
+import json
+
+with open('VideoSourceConfig.json') as f:
+    config = json.load(f)
+
+CONF = config["CONF"]
+IOU = config["IOU"]
+INPUT_SOURCE = config["InputSource"]
+
+
+def main():
+    video = cv2.VideoCapture(INPUT_SOURCE, cv2.CAP_DSHOW)
+
+    model = YOLO("res/best.pt")
+    robot_center = None
+    arrow_center = None
+    angle_deg = None
+    color = (0, 0, 255)
+    # Variables to store the closest ball and its distance to the robot
+    closest_ball = None
+    closest_ball_distance = float('inf')
+    ret, frame = video.read()
+
+    while video.isOpened():
+        ret, frame = video.read()
+        if ret:
+
+            result = model(frame, conf=CONF, iou=IOU)[0]
+            detections = sv.Detections.from_yolov8(result)
+            # Parse result image and calculate angle
+            for i in range(len(detections)):
+                xyxy = detections.xyxy[i]
+                confidence = detections.confidence[i]
+                class_id = detections.class_id[i]
+
+                center_x = (xyxy[0] + xyxy[2]) / 2
+                center_y = (xyxy[1] + xyxy[3]) / 2
+
+                start_point = (int(xyxy[0]), int(xyxy[1]))
+                end_point = (int(xyxy[2]), int(xyxy[3]))
+                # draw/place bounding boxes on image
+                # cv2.rectangle(frame, start_point, end_point, color=(0, 0, 0), thickness=2)
+
+                if np.isin(7, class_id):
+                    robot_center = (center_x, center_y)
+                    color = (0, 255, 0)  # Green for robot
+
+                elif np.isin(6, class_id):
+                    arrow_center = (center_x, center_y)
+                    color = (0, 0, 255)  # Red for front
+
+                elif np.isin(1 and 2, class_id):
+                    # Calculate the Euclidean distance between the robot and the ball
+                    if robot_center is not None:
+                        distance = math.sqrt((center_x - robot_center[0]) ** 2 + (center_y - robot_center[1]) ** 2)
+                        # If this ball is closer than the current closest ball, update the closest ball and its distance
+                        if distance < closest_ball_distance:
+                            closest_ball = (center_x, center_y)
+                            closest_ball_distance = distance
+
+            if robot_center and arrow_center:
+                # calculate differences in x and y coordinates
+                diff_x = arrow_center[0] - robot_center[0]
+                diff_y = arrow_center[1] - robot_center[1]
+
+                # calculate angle in radians
+                angle_rad = math.atan2(-diff_y, diff_x)
+
+                # convert angle to degrees
+                angle_deg = (math.degrees(angle_rad) + 360) % 360
+
+                print("Robot is facing at angle:", angle_deg, "degrees")
+
+            # Print the coordinates of the closest ball, if any
+            if closest_ball is not None:
+                print("Closest ball is at coordinates:", closest_ball)
+
+            annotated_frame = result.plot()
+
+            cv2.imshow("yolov8", annotated_frame)
+
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+    # Release the video capture object and close the display window
+    video.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
