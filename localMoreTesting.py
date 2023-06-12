6f6ffb1abf7dcc720ce9caf95f429e9e9ea59428
Haydar Hamid Abbas Bahr <haydar919@gmail.com>
2023-06-12 09:48:59 +0200

    Cleaned up code

--- localMoreTesting.py
+++ localMoreTesting.py
@@ -1,12 +1,10 @@
 import socket
-
 import cv2
-
+import json
+import math
+import numpy as np
 from ultralytics import YOLO
 import supervision as sv
-import numpy as np
-import math
-import json
 
 with open('VideoSourceConfig.json') as f:
     config = json.load(f)
@@ -14,190 +12,92 @@ with open('VideoSourceConfig.json') as f:
 CONF = config["CONF"]
 IOU = config["IOU"]
 INPUT_SOURCE = config["InputSource"]
-
-host = "192.168.43.168"  # get local machine name
-port = 1060  # Make sure it's within the > 1024 $$ <65535 range
+host, port = "192.168.43.168", 1060  # get local machine name and port
 
 
-##s = socket.socket()
-##s.connect((host, port))
-
-def move_robot(robot_angle, robot_coord, ball_coord, isMoving):
-    # target_angle = math.atan2(ball_coord[1] - robot_coord[1], ball_coord[0] - robot_coord[0]) * (180 / math.pi)
-
-    is_moving = isMoving
-
-    # calculate differences in x and y coordinates
-    diff_x = ball_coord[0] - robot_coord[0]
-    diff_y = ball_coord[1] - robot_coord[1]
-
-    # calculate angle in radians
-    angle_rad = math.atan2(-diff_y, diff_x)
-
-    # convert angle to degrees
-    target_angle = (math.degrees(angle_rad) + 360) % 360
-
-    leftDif = (target_angle - robot_angle) % 360
-    rightDif = (robot_angle - target_angle) % 360
-
-    if (robot_angle < target_angle + 5 and robot_angle > target_angle - 5):
-        message = "STOP"
-        # s.send(message.encode('utf-8'))
-        is_moving = False
-        return False
-    else:
-        if (is_moving == False):
-            if (rightDif <= leftDif):
-                message = "RIGHT"
-            else:
-                message = "LEFT"
-            # s.send(message.encode('utf-8'))
-            is_moving = True
-            return True
-
-
-def move_towards_goal(robot_angle, robot_coord, goal_coord, isMoving):
-    # Calculate angle between robot and goal
-    diff_x = goal_coord[0] - robot_coord[0]
-    diff_y = goal_coord[1] - robot_coord[1]
+def calculate_coordinates(robot_coord, target_coord):
+    diff_x = target_coord[0] - robot_coord[0]
+    diff_y = target_coord[1] - robot_coord[1]
     angle_rad = math.atan2(-diff_y, diff_x)
     target_angle = (math.degrees(angle_rad) + 360) % 360
+    return target_angle
 
-    is_moving = isMoving
 
+def move_target(robot_angle, robot_coord, target_coord, isMoving, message="STOP"):
+    target_angle = calculate_coordinates(robot_coord, target_coord)
     leftDif = (target_angle - robot_angle) % 360
     rightDif = (robot_angle - target_angle) % 360
-
-    if (robot_angle < target_angle + 5 and robot_angle > target_angle - 5):
-        message = "STOP"
-        ##s.send(message.encode('utf-8'))
-        is_moving = False
-        return False
-    else:
-        if (is_moving == False):
-            if (rightDif <= leftDif):
-                message = "RIGHT"
-            else:
-                message = "LEFT"
-            ##s.send(message.encode('utf-8'))
-            is_moving = True
-            return True
+    if robot_angle - 5 < target_angle < robot_angle + 5:
+        isMoving = False
+    elif not isMoving:
+        message = "RIGHT" if rightDif <= leftDif else "LEFT"
+        isMoving = True
+    return isMoving, message
 
 
 def main():
     video = cv2.VideoCapture(INPUT_SOURCE, cv2.CAP_DSHOW)
-
     model = YOLO("res/best.pt")
-    robot_center = None
-    arrow_center = None
-    goal = []
-    angle_deg = None
-    color = (0, 0, 255)
-    # Variables to store the closest ball and its distance to the robot
-    closest_ball = None
-    closest_ball_distance = float('inf')
-    closest_goal = None
-    closest_goal_distance = float('inf')
-    ret, frame = video.read()
-    is_moving = False
-
+    closest_ball_distance, closest_goal_distance = float('inf'), float('inf')
+    isMoving = False
+    robot_center, arrow_center, goal = None, None, None
     while video.isOpened():
         closest_ball = None
         closest_ball_distance = float('inf')
         bounds = []
         ret, frame = video.read()
         if ret:
-
             result = model(frame, conf=CONF, iou=IOU)[0]
             detections = sv.Detections.from_yolov8(result)
-            # Parse result image and calculate angle
-            for i in range(len(detections)):
-                xyxy = detections.xyxy[i]
-                confidence = detections.confidence[i]
-                class_id = detections.class_id[i]
-
-                center_x = (xyxy[0] + xyxy[2]) / 2
-                center_y = (xyxy[1] + xyxy[3]) / 2
-
-                start_point = (int(xyxy[0]), int(xyxy[1]))
-                end_point = (int(xyxy[2]), int(xyxy[3]))
-                # draw/place bounding boxes on image
-                # cv2.rectangle(frame, start_point, end_point, color=(0, 0, 0), thickness=2)
-
-                if np.isin(7, class_id):
-                    robot_center = (center_x, center_y)
-                    color = (0, 255, 0)  # Green for robot
-
-                elif np.isin(6, class_id):
-                    arrow_center = (center_x, center_y)
-                    color = (0, 0, 255)  # Red for front
-
-                elif np.isin(1 and 2, class_id):
-                    # Calculate the Euclidean distance between the robot and the ball
-                    if robot_center is not None:
-                        distance = math.sqrt((center_x - robot_center[0]) ** 2 + (center_y - robot_center[1]) ** 2)
-                        # If this ball is closer than the current closest ball, update the closest ball and its distance
-                        if distance < closest_ball_distance:
-                            closest_ball = (center_x, center_y)
-                            closest_ball_distance = distance
-
-                elif class_id == 3:
-                    x_center = (xyxy[0] + xyxy[2]) / 2  # calculate x center of the bound
-                    y_center = (xyxy[1] + xyxy[3]) / 2  # calculate y center of the bound
-                    bounds.append((x_center, y_center))  # save the x and y coordinates of the bounds
-
-            for center in bounds:
-                if 400 > center[1] > 200:
-                    goal.append(center)
-                    cv2.circle(frame, (int(center[0]), int(center[1])), radius=10, color=(0, 0, 255),
-                               thickness=-1)  # draw a circle for each bound center
-                    for goal in goals:
-                        if robot_center is not None:
-                            distance = math.sqrt((goal[0] - robot_center[0]) ** 2 + (goal[1] - robot_center[1]) ** 2)
-                            if distance < closest_goal_distance:
-                                closest_goal = goal
-                                closest_goal_distance = distance
-
-            if robot_center and arrow_center:
-                # calculate differences in x and y coordinates
-                diff_x = arrow_center[0] - robot_center[0]
-                diff_y = arrow_center[1] - robot_center[1]
-
-                # calculate angle in radians
-                angle_rad = math.atan2(-diff_y, diff_x)
-
-                # convert angle to degrees
-                angle_deg = (math.degrees(angle_rad) + 360) % 360
-
-                print("Robot is facing at angle:", angle_deg, "degrees")
-
-            # Print the coordinates of the closest ball, if any
-            if closest_ball is not None:
-                print("Closest ball is at coordinates:", closest_ball)
-            if closest_goal is not None:
-                print("Closest goal is at coordinates:", closest_goal)
-
-            annotated_frame = result.plot()
-
+            robot_center, arrow_center, closest_ball = handle_detections(detections, robot_center, arrow_center,
+                                                                         closest_ball, closest_ball_distance)
+            goal = find_goal(goal, bounds)
+            angle_deg = find_robot_angle(robot_center, arrow_center)
+            if goal is not None:
+                cv2.circle(frame, (int(goal[0]), int(goal[1])), radius=10, color=(0, 0, 255),
+                           thickness=-1)
             cv2.imshow("yolov8", frame)
-            print(goal[0], goal[1])
-
-            # Break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
-                ##if closest_ball is not None and robot_center is not None and angle_deg is not None:
-                ##  is_moving = move_robot(angle_deg,robot_center,closest_ball,is_moving)
-                # Move towards goal after the robot has picked up the closest ball
-                ##if goal is not None:
-                ## is_moving = move_towards_goal(angle_deg, robot_center, goal, is_moving)
-                ##else:
-                message = "STOP"
-            ## s.send(message.encode('utf-8'))
-
-    # Release the video capture object and close the display window
     video.release()
     cv2.destroyAllWindows()
-    ##s.close()
+
+
+def handle_detections(detections, robot_center, arrow_center, closest_ball, closest_ball_distance):
+    for i in range(len(detections)):
+        xyxy = detections.xyxy[i]
+        center_x = (xyxy[0] + xyxy[2]) / 2
+        center_y = (xyxy[1] + xyxy[3]) / 2
+        class_id = detections.class_id[i]
+        if class_id == 7:
+            robot_center = (center_x, center_y)
+        elif class_id == 6:
+            arrow_center = (center_x, center_y)
+        elif class_id in [1, 2]:
+            if robot_center is not None:
+                distance = math.sqrt((center_x - robot_center[0]) ** 2 + (center_y - robot_center[1]) ** 2)
+                if distance < closest_ball_distance:
+                    closest_ball = (center_x, center_y)
+                    closest_ball_distance = distance
+    return robot_center, arrow_center, closest_ball
+
+
+def find_goal(goal, bounds):
+    for center in bounds:
+        if 200 < center[1] < 400 and center[0] < 200 and goal is None:
+            goal = (center[0], center[1])
+    return goal
+
+
+def find_robot_angle(robot_center, arrow_center):
+    if robot_center and arrow_center:
+        diff_x = arrow_center[0] - robot_center[0]
+        diff_y = arrow_center[1] - robot_center[1]
+        angle_rad = math.atan2(-diff_y, diff_x)
+        angle_deg = (math.degrees(angle_rad) + 360) % 360
+        print("Robot is facing at angle:", angle_deg, "degrees")
+        return angle_deg
+    return None
 
 
 if __name__ == "__main__":
